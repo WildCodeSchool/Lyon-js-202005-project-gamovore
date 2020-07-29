0fd369d0fbb20685d90573953e92dba388b632a3
P-P-P-Pablo <pvilella.pro@gmail.com>
2020-07-29 11:11:49 +0200

    main component added

---
+++ src/components/Main.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Main.css';
+
+function Main() {
+  return (
+    <div className="Main">
+        <img src='https://giphy.com/gifs/3oriff4xQ7Oq2TIgTu/html5'></img>
+    </div>
+  );
+}
+
+export default Main;
