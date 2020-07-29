646387c2fdcaea2861d8703ef863a131fe3dd8e3
alyson-b69 <alyson.bernabeu@gmail.com>
2020-07-29 17:04:38 +0200

    fix bug in ul and li components

--- src/components/Ul.js
+++ src/components/Ul.js
@@ -5,20 +5,31 @@ const Ul = styled.ul`
   text-transform:uppercase;
   margin:0;
   padding:0;
+ 
+  
 
   & li:first-child {
        background-color: #FBB700;
-       padding:1%
+       color: #393431;
+       font-weight:bold;
+       font-size:1.5em;
+       padding-left:5%;
     }
 
   & ul li:first-child {
       background-color: #2E2A27;
       color:#F3F3F3;
+
   }
 
   & ul ul li:first-child {
       background-color: #393431;
-      color:#F3F3F3;
+      color: #F3F3F3;
+      font-weight:normal;
+      font-size:1em;
+      padding-left: 0;
+      padding-left:10%;
+
   }
 `;
 
