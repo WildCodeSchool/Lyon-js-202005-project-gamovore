1ea530e54b13fed4a12acef397c1e97cca150d77
alyson-b69 <alyson.bernabeu@gmail.com>
2020-07-30 08:36:46 +0200

    rename Ul Li components for the sidebarMenu

--- src/components/SidebarItemMenu.js
+++ src/components/SidebarItemMenu.js
@@ -1,10 +1,10 @@
 import styled from "styled-components";
 
-const Li = styled.li`
+const SidebarItemMenu = styled.li`
     color: #F3F3F3;
     padding-left:10%;
     padding-top:2%;
     padding-bottom:2%
 `;
 
-export default Li;
+export default SidebarItemMenu;
