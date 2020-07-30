c193cb948c6842f86792bf53144a0144e4d28124
alyson-b69 <alyson.bernabeu@gmail.com>
2020-07-30 17:18:16 +0200

    supp import react router de sidebar and add profilbutton to header to see this

--- src/boxbox/HeaderBox.js
+++ src/boxbox/HeaderBox.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import logoGamovore from '../logogamovorebrown.png';
 import Header from '../components/Header';
+import ProfilButton from '../components/ProfilButton';
 
 function HeaderBox () {
     return (
         <Header>
             <img src={logoGamovore} alt="logo_gamovore" width="85px" height="81px"/>
             <h1>Gamovore</h1>
-            <ProfilButton/>
+            <ProfilButton />
         </Header>
     );
 }
