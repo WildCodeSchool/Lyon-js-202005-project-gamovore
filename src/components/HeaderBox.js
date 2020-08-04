504e0568d6626cd4737c3748759e44ea3a8ad9ce
alyson-b69 <alyson.bernabeu@gmail.com>
2020-08-05 00:39:16 +0200

    create button from sign in and sign up to the header

--- src/components/HeaderBox.js
+++ src/components/HeaderBox.js
@@ -1,14 +1,22 @@
 import React from 'react';
+import {Link} from 'react-router-dom';
 import logoGamovore from '../logogamovorebrown.png';
 import Header from './Header';
-import ProfilButton from './ProfilButton'
+import ProfilButton from "./ProfilButton";
+import Button from "./Button";
+import Linked from "./Linked";
 
 function HeaderBox () {
     return (
         <Header>
             <img src={logoGamovore} alt="logo_gamovore" width="85px" height="81px"/>
             <h1>Gamovore</h1>
+            <>
+            <Linked to="/sign-in"><Button>Connexion</Button></Linked>
+            <Linked to="/sign-up"><Button>Inscription</Button></Linked>
             <ProfilButton/>
+            </>
+           
         </Header>
     );
 }
