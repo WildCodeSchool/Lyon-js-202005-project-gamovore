fe24d32cd34eeff028dca07b0df9b638126c9499
Seblecaribou <morin.seb.89@gmail.com>
2020-08-05 11:00:44 +0200

    Added API call

--- src/components/GameListBox.js
+++ src/components/GameListBox.js
@@ -1,12 +1,30 @@
 import React from "react";
 
 const GameListBox = () => {
+  const [game, setGame]=useState([]);
+  axios({
+      url: "https://cors-anywhere.herokuapp.com/https://api-v3.igdb.com/games",
+      method: "POST",
+      headers: {
+        Accept: "application/json",
+        "user-key":process.env.REACT_APP_API_KEY,
+      },
+      data: `fields name, cover.url; limit 20;`,
+    })
+      .then((response) => response.data)
+      .then((data) => {
+        setGame(data);
+      })
+      .catch((err) => {
+        console.error(err);
+      });
+      
+  };
+
 
   return (
     <div>
        
-      <p>Welcome to the Game List</p>
-
     </div>
   );
 };
