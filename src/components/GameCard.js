2e42dbef2c40b4b343bdef77d333e1534227b4bd
P-P-P-Pablo <pvilella.pro@gmail.com>
2020-08-25 10:14:39 +0200

    update button in black because it's coooler

--- src/components/GameCard.js
+++ src/components/GameCard.js
@@ -7,41 +7,41 @@ import GameCardName from "../style/GameCardName";
 import GameCardStyle from "../style/GameCardStyle";
 import AddGameButton from "../style/AddGameButton";
 import Plus from "../style/Plus";
-import PlusImg from "../img/white/plus.png";
+import PlusImg from "../img/black/plus.png";
 import gameCoverPlaceholder from "../img/white/gameCoverPlaceholder.png";
 
 const GameCard = (props) => {
-  // const { currentUser, setCurrentUser } = useContext(AuthContext);
-  // const gameId = props.id;
+	// const { currentUser, setCurrentUser } = useContext(AuthContext);
+	// const gameId = props.id;
 
-  // const addGame = () => {
-  //   if (currentUser.favoriteGameId.indexOf(gameId) === -1) {
-  //     setCurrentUser({
-  //       ...currentUser,
-  //       favoriteGameId: [...currentUser.favoriteGameId, gameId],
-  //     });
-  //   } else {
-  //     alert("Game is already in your favorites" + gameId);
-  //   }
-  // };
+	// const addGame = () => {
+	//   if (currentUser.favoriteGameId.indexOf(gameId) === -1) {
+	//     setCurrentUser({
+	//       ...currentUser,
+	//       favoriteGameId: [...currentUser.favoriteGameId, gameId],
+	//     });
+	//   } else {
+	//     alert("Game is already in your favorites" + gameId);
+	//   }
+	// };
 
-  return (
-    <GameCardStyle>
-      <GameCardJacquette
-        src={
-          props.cover
-            ? "https:" + props.cover.url.replace("thumb", "cover_big_2x")
-            : gameCoverPlaceholder
-        }
-      />
-      <GameCardName>{props.name}</GameCardName>
-      {/*onClick={addGame}*/}
-      <AddGameButton>
-        <Plus src={PlusImg} />
-        Add to Collection
-      </AddGameButton>
-    </GameCardStyle>
-  );
+	return (
+		<GameCardStyle>
+			<GameCardJacquette
+				src={
+					props.cover
+						? "https:" + props.cover.url.replace("thumb", "cover_big_2x")
+						: gameCoverPlaceholder
+				}
+			/>
+			<GameCardName>{props.name}</GameCardName>
+			{/*onClick={addGame}*/}
+			<AddGameButton>
+				<Plus src={PlusImg} />
+				Add to Collection
+			</AddGameButton>
+		</GameCardStyle>
+	);
 };
 
 export default GameCard;
--- src/style/Plus.js
+++ src/style/Plus.js
@@ -2,10 +2,10 @@ import styled from "styled-components";
 // import { render } from '@testing-library/react';
 
 const Plus = styled.img`
-  border-radius: 5%;
-  margin: 0;
-  width: 20%;
-  border-right: 1.5px dotted white;
+	border-radius: 5%;
+	margin: 0;
+	width: 20%;
+	border-right: 1.5px dotted black;
 `;
 
 export default Plus;
