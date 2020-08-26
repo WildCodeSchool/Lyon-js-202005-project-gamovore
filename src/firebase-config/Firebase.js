dc8522678376b6e283337e411867c41a6f03333e
EthernalSlayer <guillaume.b.a@protonmail.com>
2020-08-26 10:22:25 +0200

    correct security failure

--- src/firebase-config/Firebase.js
+++ src/firebase-config/Firebase.js
@@ -2,13 +2,13 @@ import app from "firebase/app";
 import "firebase/auth";
 
 const config = {
-  apiKey: "AIzaSyDuZ7SjWW2gHhzyy2VbWAhoLSCDFsLKBbY",
-  authDomain: "gamovore-app.firebaseapp.com",
-  databaseURL: "https://gamovore-app.firebaseio.com",
-  projectId: "gamovore-app",
-  storageBucket: "gamovore-app.appspot.com",
-  messagingSenderId: "373025067815",
-  appId: "1:373025067815:web:0dcea5ad7cfb3b3a1e0f02",
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
 };
 
 class Firebase {
