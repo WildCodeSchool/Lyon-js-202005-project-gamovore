8a7347962686cc578335f3247fc76f8b3dcd3965
P-P-P-Pablo <pvilella.pro@gmail.com>
2020-08-26 16:46:21 +0200

    added a title for every pages

---
+++ src/components/PageTitle.js
@@ -0,0 +1,29 @@
+import React from "react";
+import { useLocation } from "react-router-dom";
+import StyledPageTitle from "../style/StyledPageTitle";
+
+export default function PageTitle() {
+	const location = useLocation();
+	const url = location.pathname;
+	function mySwitch(url) {
+		if (url.includes("sign-in")) {
+			return "Sign In";
+		} else if (url.includes("sign-up")) {
+			return "Sign Up";
+		} else if (url.includes("profil")) {
+			return "Profile";
+		} else if (url.includes("game")) {
+			return "Game Details";
+		} else {
+			return "Games List";
+		}
+	}
+
+	return (
+		<div>
+			<StyledPageTitle>
+				<h1>{mySwitch(url)}</h1>
+			</StyledPageTitle>
+		</div>
+	);
+}
