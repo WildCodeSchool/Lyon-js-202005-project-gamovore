3306222b33ebcd47f3cc92656fa69f351ed4487b
alyson-b69 <alyson.bernabeu@gmail.com>
2020-09-03 12:26:10 +0200

    change card style to have same height on image card

--- src/style/GameCardName.js
+++ src/style/GameCardName.js
@@ -2,7 +2,6 @@ import styled from "styled-components";
 
 const GameCardName = styled.p`
   display: flex;
-  margin: 2%;
   color: #eee;
   font-weight: 500;
   font-size: 1.2em;
--- src/style/ImageContent.js
+++ src/style/ImageContent.js
@@ -1,10 +1,10 @@
 import styled from "styled-components";
 
-const GameCardJacquette = styled.img`
+const ImageContent = styled.div`
   border-top-right-radius: 20px;
   border-top-left-radius: 20px;
-  max-height: 100%;
   width: 100%;
+  height: 60%;
 `;
 
-export default GameCardJacquette;
+export default ImageContent;
