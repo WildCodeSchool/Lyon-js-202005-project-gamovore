87470ed03c7335aaa8731d5b45efdd66faf12467
Seblecaribou <morin.seb.89@gmail.com>
2020-09-08 16:44:42 +0200

    Made the logo SHINE AS F*CK!

--- src/components/HeaderBox.js
+++ src/components/HeaderBox.js
@@ -1,8 +1,9 @@
 import React, { useContext } from "react";
 import { UserContext } from "../context/UserContext";
 import { GameListContext } from "../context/GameListContext";
-
 import logoGamovore from "../logogamovorebrown.png";
+import LogoShining from "../style/LogoShining";
+import LogoImg from "../style/LogoImg";
 import Header from "../style/Header";
 import ProfilButton from "./ProfilButton";
 import Button from "../style/Button";
@@ -24,7 +25,9 @@ function HeaderBox() {
     <Header>
       <Linked to="/" onClick={onClick}>
         <Identity>
-          <img src={logoGamovore} alt="logo_gamovore" width="10%" />
+          <LogoShining>
+            <LogoImg src={logoGamovore} alt="logo_gamovore" />
+          </LogoShining>
           <Title>Gamovore</Title>
         </Identity>
       </Linked>
