d3d4645179171d423e18783fcae7e34be7c80600
alyson-b69 <alyson.bernabeu@gmail.com>
2020-09-10 10:46:28 +0200

    delete import propchektype on logout - auto writing

--- src/components/Logout.js
+++ src/components/Logout.js
@@ -2,7 +2,6 @@ import React, { useContext } from "react";
 import FirebaseContext from "../firebase-config/FirebaseContext";
 import Button from "../style/Button";
 import { UserContext } from "../context/UserContext";
-import { checkPropTypes } from "prop-types";
 
 const Logout = () => {
   const firebase = useContext(FirebaseContext);
