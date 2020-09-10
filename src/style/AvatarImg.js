a8046071aa712b4198cef27def9c448983de91f3
alyson-b69 <alyson.bernabeu@gmail.com>
2020-09-10 15:32:15 +0200

    change avatar image on profil

--- src/style/AvatarImg.js
+++ src/style/AvatarImg.js
@@ -3,8 +3,9 @@ import styled from "styled-components";
 const AvatarImg = styled.img`
   border-radius: 50%;
   margin-right: 1em;
-  width: 15%;
-  height: auto;
+  width: 15vh;
+  height: 15vh;
+  object-fit: cover;
 `;
 
 export default AvatarImg;
