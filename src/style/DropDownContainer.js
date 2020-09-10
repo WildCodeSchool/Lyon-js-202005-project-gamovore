b5f25f7b1e97930b4b723f9e382c52e4b91e7f34
Seblecaribou <morin.seb.89@gmail.com>
2020-09-10 16:05:29 +0200

    Added invisible div to help the dropdown connection

--- src/style/DropDownContainer.js
+++ src/style/DropDownContainer.js
@@ -1,16 +1,12 @@
 import styled from "styled-components";
 
 const DropDownContainer = styled.div`
-  position: absolute;
-  top: 120px;
+  position: relative;
+  top: 35px;
   padding-bottom: 1%;
+  object-fit: cover;
   background-color: #2e2a27;
   box-shadow: 0 8px 8px #000000;
-  width: 20%;
-  @media screen and (max-width: 600px) {
-    top: 170px;
-    width: 30%;
-  }
 `;
 
 export default DropDownContainer;
---
+++ src/style/DropDownContainerInvisible.js
@@ -0,0 +1,15 @@
+import styled from "styled-components";
+import DropDownContainer from "./DropDownContainer";
+
+const DropDownContainerInvisible = styled.div`
+  background-color: rgba(0, 0, 0, 0);
+  position: absolute;
+  top: 90px;
+  width: 20%;
+  @media screen and (max-width: 600px) {
+    top: 170px;
+    width: 30%;
+  }
+`;
+
+export default DropDownContainerInvisible;
