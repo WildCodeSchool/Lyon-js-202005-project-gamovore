9cbed025e6a921448a1ff4310e71aa916697b213
alyson-b69 <alyson.bernabeu@gmail.com>
2020-09-10 14:11:31 +0200

    change link for the footer

--- src/components/FooterBox.js
+++ src/components/FooterBox.js
@@ -1,6 +1,6 @@
 import React from "react";
 import Footer from "../style/Footer";
-import Linked from "../style/Linked";
+import ExtLink from "../style/ExtLink";
 import FooterSpan from "../style/FooterSpan";
 import ScrollButton from "../components/ScrollButton";
 
@@ -14,25 +14,40 @@ const FooterBox = () => {
         </span>
       </FooterSpan>
       by{" "}
-      <Linked to="https://www.linkedin.com/in/alexandra-lhermitte-802a54171/">
+      <ExtLink
+        href="https://www.linkedin.com/in/alexandra-lhermitte-802a54171/"
+        title="Alexandra Linkedin"
+      >
         Alexandra
-      </Linked>
+      </ExtLink>
       ,{" "}
-      <Linked to="https://www.linkedin.com/in/alyson-bernabeu-08249a172/">
+      <ExtLink
+        href="https://www.linkedin.com/in/alyson-bernabeu-08249a172/"
+        title="Alyson Linkedin"
+      >
         Alyson
-      </Linked>
+      </ExtLink>
       ,{" "}
-      <Linked to="https://www.linkedin.com/in/guillaume-bento-aires-7623071a3/">
+      <ExtLink
+        href="https://www.linkedin.com/in/guillaume-bento-aires-7623071a3/"
+        title="guillaume Linkedin"
+      >
         Guillaume
-      </Linked>
+      </ExtLink>
       ,{" "}
-      <Linked to="https://www.linkedin.com/in/pablo-vilella-0bb66b195/">
+      <ExtLink
+        href="https://www.linkedin.com/in/pablo-vilella-0bb66b195/"
+        title="Pablo Linkedin"
+      >
         Pablo
-      </Linked>{" "}
+      </ExtLink>{" "}
       et{" "}
-      <Linked to="https://www.linkedin.com/in/s%C3%A9bastien-morin-70a0371ab/">
+      <ExtLink
+        href="https://www.linkedin.com/in/s%C3%A9bastien-morin-70a0371ab/"
+        title="Sebastien Linkedin"
+      >
         Sebastien
-      </Linked>
+      </ExtLink>
       <ScrollButton scrollStepInPx="50" delayInMs="10" />
     </Footer>
   );
